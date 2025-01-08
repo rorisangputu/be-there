9804f62279ce3ef1c2a1159b395f3b10d15950e3
Rorisang Putu <rorisangputu@gmail.com>
2025-01-08 20:18:46 +0200

    creating user schema

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -2,4 +2,24 @@ import mongoose, { Schema } from "mongoose";
 import bcrypt from 'bcryptjs';
 import { UserType } from "../shared/types";
 
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+});
+
 
