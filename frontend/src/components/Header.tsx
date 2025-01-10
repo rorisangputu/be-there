f662aa03ea98379e44b86373094896896f741e99
Rorisang Putu <rorisangputu@gmail.com>
2025-01-10 23:09:23 +0200

    spacing nav components

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,21 +2,26 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="w-full p-3 border">
-      <div className="w-[90%] mx-auto border flex flex-row justify-between">
+    <div className="w-full my-6">
+      <div className="w-[90%] mx-auto flex flex-row justify-between items-center">
         <div>
           <Link to={"/"}>
-            <h1 className="font-bold text-lg text-[#181917]">b/there</h1>
+            <h1 className="font-bold text-2xl text-[#181917]">b/there</h1>
           </Link>
         </div>
-        <div>
-          <ul className="flex flex-row gap-3">
+              <div className="flex flex-row gap-3 items-center">
+                  
+                  
+          <ul className="hidden md:flex flex-row gap-3">
             {links.map((link) => (
               <li key={link.url}>
                 <Link to={link.url}>{link.title}</Link>
               </li>
             ))}
           </ul>
+          <Link to={"/sign-in"}>
+            <span className="bg-[#fefae0] p-3 font-semibold">Log In</span>
+          </Link>
         </div>
       </div>
     </div>
