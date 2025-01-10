79700f839ff7fd14ef693f52e7b8fa12d016fe14
Rorisang Putu <rorisangputu@gmail.com>
2025-01-10 22:45:04 +0200

    adding header component to layout

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,9 +1,12 @@
+import Header from "../components/Header";
+
 interface LayoutProps {
   children: React.ReactNode;
 }
 const Layout = ({ children }: LayoutProps) => {
   return (
     <div>
+      <Header />
       <div>{children}</div>
     </div>
   );
