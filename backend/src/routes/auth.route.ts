7a457f91f05ebf1acd2de2660423cc9ff5c70cb2
Rorisang Putu <rorisangputu@gmail.com>
2025-01-13 19:26:45 +0200

    creating tokem validation route

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,5 +1,14 @@
-import express, {Request, Response} from 'express'
+import {Request, Response, Router} from 'express'
 import { check, validationResult } from 'express-validator'
 import User from '../models/user.model';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import { verifyToken } from '../middleware/auth.middleware';
+
+const router = Router()
+router.get("/validate-token", verifyToken, (req: Request, res: Response) => {
+    res.status(200).json({ userId: req.userId })
+    return;
+});
+
+export default router;
