b870dc9f54dd9d89bd70427dcbf5ad88c3bf4028
Rorisang Putu <rorisangputu@gmail.com>
2025-01-13 19:09:36 +0200

    adding register form data type

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,32 +1,67 @@
 import { Link } from "react-router-dom";
 
+export type RegisterFormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
 const Register = () => {
   return (
     <div className="w-full">
       <div className="w-[90%] mx-auto py-20">
-        <div className="flex flex-col p-10 bg-white justify-center items-center gap-5">
-          <h1 className="text-center text-2xl">Register</h1>
+        <div className="flex flex-col py-10 bg-white justify-center items-center gap-5">
+          <h1 className="text-center text-3xl font-bold">Register</h1>
           <form className="flex flex-col gap-5">
-            <span className="flex gap-2">
-              Username:
+            <div className="flex flex-col md:flex-row gap-5">
+              <span className="flex gap-2 flex-col text-slate-500">
+                First Name:
+                <input
+                  className="bg-gray-100 border-slate-500 border focus:outline-none p-2"
+                  type="text"
+                />
+              </span>
+              <span className="flex gap-2 flex-col text-slate-500">
+                Last Name:
+                <input
+                  className="bg-gray-100 border-slate-500 border focus:outline-none p-2"
+                  type="text"
+                />
+              </span>
+            </div>
+            <span className="flex gap-2 flex-col text-slate-500">
+              Email:
               <input
-                className="bg-gray-100 border-slate-500 border focus:outline-none"
-                type="text"
+                className="bg-gray-100 border-slate-500 border focus:outline-none p-2"
+                type="email"
               />
             </span>
-            <span className="flex gap-2">
-              Password:
-              <input
-                className="bg-gray-100 border-slate-500 border focus:outline-none"
-                type="text"
-              />
-            </span>
+            <div className="flex flex-col md:flex-row gap-5">
+              <span className="flex gap-2 flex-col text-slate-500">
+                Password:
+                <input
+                  className="bg-gray-100 border-slate-500 border focus:outline-none p-2"
+                  type="password"
+                />
+              </span>
+              <span className="flex gap-2 flex-col text-slate-500">
+                Confirm Password:
+                <input
+                  className="bg-gray-100 border-slate-500 border focus:outline-none p-2"
+                  type="password"
+                />
+              </span>
+            </div>
             <span className="flex justify-end">
-              <button className="p-2 text-white bg-[#181917]">Log In</button>
+              <button className="p-3 text-white bg-[#181917]">Register</button>
             </span>
           </form>
           <div className="flex gap-2 my-5">
-            <Link to={"/sign-in"}>Log In</Link>
+            <Link className="cursor-pointer" to={"/sign-in"}>
+              Have an account? Log In
+            </Link>
           </div>
         </div>
       </div>
