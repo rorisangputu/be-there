6bf79b5059d54614b000b1e7c5a929d85c387e74
Rorisang Putu <rorisangputu@gmail.com>
2025-01-13 18:00:08 +0200

    adding express validator checks on register route

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -6,8 +6,17 @@ import { verifyToken } from '../middleware/auth.middleware';
 
 const router = Router();
 
-router.get('/ola', async (req: Request, res: Response) => {
-    res.send({ message: "HELLLLOO from User route! " })
-});
+// router.get('/ola', async (req: Request, res: Response) => {
+//     res.send({ message: "HELLLLOO from User route! " })
+// });
+
+router.post("/register", [
+    check("firstName", "First Name is required").isString(),
+    check("lastName", "Last Name is required").isString(),
+    check("email", "Email is required").isString(),
+    check("password", "Password must be 6 or more characters").isLength({
+        min: 6
+    }),
+])
 
 export default router;
