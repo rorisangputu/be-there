57fa109d9f649ae72e2095a7afbabbe7f9e60d5f
Rorisang Putu <rorisangputu@gmail.com>
2025-01-13 19:39:56 +0200

    adding app context to app

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -1,17 +1,54 @@
-import React, { useContext, useState } from 'react';
-import { useQuery } from 'react-query';
-import * as apiClient from '../apiClient';
+import React, { useContext, useState } from "react";
+import { useQuery } from "react-query";
+import * as apiClient from "../apiClient";
+import Toast from "../components/Toast";
 
 type ToastMessage = {
-    message: string;
-    type: "SUCCESS" | "ERROR";
+  message: string;
+  type: "SUCCESS" | "ERROR";
 };
 
 type AppContext = {
-    showToast: (toastMessage: ToastMessage) => void;
-    isLoggedIn: boolean;
-}
+  showToast: (toastMessage: ToastMessage) => void;
+  isLoggedIn: boolean;
+};
 
 const AppContext = React.createContext<AppContext | undefined>(undefined);
 
-export
+export const AppContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  //Declaring state object that holds state of toast
+  const [toast, setToast] = useState<ToastMessage | undefined>(undefined);
+
+  const { isError } = useQuery("validateToken", apiClient.validateToken, {
+    retry: false,
+  });
+  return (
+    <AppContext.Provider
+      value={{
+        showToast: (toastMessage) => {
+          setToast(toastMessage);
+        },
+        isLoggedIn: !isError,
+      }}
+    >
+      {toast && (
+        <Toast
+          message={toast.message}
+          type={toast.type}
+          onClose={() => setToast(undefined)}
+        />
+      )}
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+//Hook allows components to access context
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+  return context as AppContext;
+};
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,25 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
+import { AppContextProvider } from "./contexts/AppContext.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
