16df076ca113356bb13e15432949ab424e71ffa8
Rorisang Putu <rorisangputu@gmail.com>
2025-01-13 18:32:42 +0200

    creating register page

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  return (
+    <div className="w-full">
+      <div className="w-[90%] mx-auto py-20">
+        <div className="flex flex-col p-10 bg-white justify-center items-center gap-5">
+          <h1 className="text-center text-2xl">Register</h1>
+          <form className="flex flex-col gap-5">
+            <span className="flex gap-2">
+              Username:
+              <input
+                className="bg-gray-100 border-slate-500 border focus:outline-none"
+                type="text"
+              />
+            </span>
+            <span className="flex gap-2">
+              Password:
+              <input
+                className="bg-gray-100 border-slate-500 border focus:outline-none"
+                type="text"
+              />
+            </span>
+            <span className="flex justify-end">
+              <button className="p-2 text-white bg-[#181917]">Log In</button>
+            </span>
+          </form>
+          <div className="flex gap-2 my-5">
+            <Link to={"/sign-in"}>Log In</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
