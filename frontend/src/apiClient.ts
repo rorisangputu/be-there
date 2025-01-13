ad029108247f73697469bded8386306674e9bdb5
Rorisang Putu <rorisangputu@gmail.com>
2025-01-13 19:23:46 +0200

    adding jwt validation request

--- frontend/src/apiClient.ts
+++ frontend/src/apiClient.ts
@@ -17,3 +17,14 @@ export const regsiter = async (formData: RegisterFormData) => {
         throw new Error(responseBody.message);
     }
 }
+
+export const validateToken = async () => {
+    const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
+        credentials: "include"
+    });
+
+    if (!response.ok) {
+        throw new Error("Token invalid")
+    }
+    return response.json();
+};
