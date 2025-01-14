f4e1d7e360bc1d631676144fd9865a2439a9f95f
Rorisang Putu <rorisangputu@gmail.com>
2025-01-14 22:50:54 +0200

    styling profile page

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,9 +1,34 @@
-
+import { useQuery } from "react-query";
+import * as apiClient from "../apiClient";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
+  const { data: currentUser } = useQuery(
+    "fetchCurrentUser",
+    apiClient.fetchCurrentUser
+  );
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="w-full">
+      <div className="w-[90%] mx-auto">
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="text-2xl">
+              {currentUser?.firstName} {currentUser?.lastName}
+            </h1>
+            <p>{currentUser?.email}</p>
+          </div>
+          <div>
+            <Link
+              to={`/edit-profile/${currentUser?._id}`}
+              className="text-xl bg-[#181917] text-white p-3 hover:bg-[#323530]"
+            >
+              Edit Profile
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
