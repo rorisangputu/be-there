40dc5eec6d8955b0d0f32f5f1711d278fc97a0de
Rorisang Putu <rorisangputu@gmail.com>
2025-01-14 14:14:23 +0200

    creating sign in function

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -6,6 +6,55 @@ import jwt from 'jsonwebtoken';
 import { verifyToken } from '../middleware/auth.middleware';
 
 const router = Router()
+
+router.post("/login", [
+    check("email", "Email is required").isEmail(),
+    check("password", "Password with 6 or more character is required").isLength({
+        min: 6
+    })
+],
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).json({ message: errors.array() });
+            return;
+        }
+
+        const { email, password } = req.body;
+
+        try {
+            const user = await User.findOne({ email });
+            if (!user) {
+                res.status(400).json({ message: "Invalid credentials" });
+                return;
+            } 
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                res.status(400).json({ message: "Invalid Credentials" });
+                return;
+            } 
+
+            const token = jwt.sign(
+                { userId: user.id },
+                process.env.JWT_SECRET_KEY as string,
+                { expiresIn: "1d" }
+            );
+            res.cookie("bethere_auth_token", token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                maxAge: 86400000
+            });
+            res.status(200).json({ userId: user._id });
+            
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: "Something went wrong" })
+        }
+        
+    }
+);
+
 router.get("/validate-token", verifyToken, (req: Request, res: Response) => {
     res.status(200).json({ userId: req.userId })
     return;
