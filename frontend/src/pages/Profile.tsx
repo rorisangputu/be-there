22f1e56b7fecd22412ccc872cac65d9957c7ac26
Rorisang Putu <rorisangputu@gmail.com>
2025-01-14 23:29:04 +0200

    styling

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -10,7 +10,7 @@ const Profile = () => {
   return (
     <div className="w-full">
       <div className="w-[90%] mx-auto">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center py-10">
           <div>
             <h1 className="text-2xl">
               {currentUser?.firstName} {currentUser?.lastName}
@@ -23,6 +23,20 @@ const Profile = () => {
               className="text-xl bg-[#181917] text-white p-3 hover:bg-[#323530]"
             >
               Edit Profile
+            </Link>
+          </div>
+        </div>
+
+        <hr className="h-[2px] border-none"></hr>
+
+        <div className="py-5">
+          <div className="flex flex-col gap-2">
+            <h2 className="text-2xl font-semibold">My Events</h2>
+            <Link
+              to={"/create"}
+              className="p-3 font-semibold text-lg bg-[#fefae0] hover:bg-[#f6f1d4] w-fit"
+            >
+              Create Event
             </Link>
           </div>
         </div>
