e51fdcaa00d1ec7855480c4f1cc48fe16b4bcffd
Rorisang Putu <rorisangputu@gmail.com>
2025-01-15 22:16:53 +0200

    adding additional inputs for create form

--- frontend/src/pages/Create.tsx
+++ frontend/src/pages/Create.tsx
@@ -1,3 +1,6 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 const Create = () => {
   return (
     <div className="w-full bg-white">
@@ -9,7 +12,10 @@ const Create = () => {
           <form action="" className="flex flex-col gap-4">
             <label htmlFor="" className="flex flex-col text-lg">
               Name:
-              <input type="text" className="border border-slate-500 w-fit" />
+              <input
+                type="text"
+                className="border border-slate-500 w-fit p-2"
+              />
             </label>
             <label htmlFor="" className="flex flex-col text-lg">
               Description:
@@ -20,8 +26,49 @@ const Create = () => {
                 cols={10}
                 rows={7}
               ></textarea>
-                      </label>
-                      
+            </label>
+            <div className="flex flex-col md:flex-row gap-5">
+              <label htmlFor="" className="flex flex-col text-lg">
+                Date:
+                <input
+                  type="date"
+                  className="border border-slate-500 w-fit p-2"
+                  name=""
+                  id=""
+                />
+              </label>
+              <label htmlFor="" className="flex flex-col text-lg">
+                Time:
+                <input
+                  type="time"
+                  className="border border-slate-500 w-fit p-2"
+                  name=""
+                  id=""
+                />
+              </label>
+            </div>
+            <label htmlFor="" className="flex flex-col text-lg">
+              Location:
+              <input
+                type="text"
+                className="border border-slate-500 w-fit p-2"
+              />
+            </label>
+            <label htmlFor="" className="flex flex-col text-lg">
+              Event Photo:
+              <input
+                type="file"
+                className="border border-slate-500 w-fit p-2"
+              />
+            </label>
+            <span className="flex justify-end my-5">
+              <button
+                type="submit"
+                className="p-3 text-lg bg-[#181917] text-white"
+              >
+                Submit
+              </button>
+            </span>
           </form>
         </div>
       </div>
