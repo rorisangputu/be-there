555d4ed64b35f858f88f442ddd8c4d59e1b45eae
Rorisang Putu <rorisangputu@gmail.com>
2025-01-16 22:41:16 +0200

    adding my event routes to app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -34,6 +34,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/my-events", myEventRoutes);
 
 //App listener
 app.listen(PORT, () => {
