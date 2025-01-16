ac469a32974b625152f712b0521a9aeace0b46f4
Rorisang Putu <rorisangputu@gmail.com>
2025-01-16 22:56:48 +0200

    validating request body with express validator

--- backend/src/routes/my-event.route.ts
+++ backend/src/routes/my-event.route.ts
@@ -1,8 +1,29 @@
 import { Router, Request, Response } from 'express';
+import multer from 'multer';
+import { verifyToken } from '../middleware/auth.middleware';
+import { body } from 'express-validator';
 
 const router = Router();
 
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024 //5mb
+    }
+});
 
+// api/my-event -- create event
+router.post('/', verifyToken,
+    [
+        body("name").notEmpty().withMessage("Name is required"),
+        body("date").notEmpty().withMessage("Date is required"),
+        body("location").notEmpty().withMessage("Location is required"),
+    ],
+    upload.single("bannerPhoto"),
+    async (req: Request, res: Response) => {
+    console.log(req.body);
+})
 
 
 export default router;
