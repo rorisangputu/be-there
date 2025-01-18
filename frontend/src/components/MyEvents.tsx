581a393d3408037bf73a30f5f254e7434fbf94fd
Rorisang Putu <rorisangputu@gmail.com>
2025-01-18 13:06:03 +0200

    implementing useQuery to fetch data from api client fetch request methods

--- frontend/src/components/MyEvents.tsx
+++ frontend/src/components/MyEvents.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { useQuery } from "react-query";
+import * as apiClient from "../apiClient";
+import { useAppContext } from "../contexts/AppContext";
 
 const MyEvents = () => {
-  return (
-    <div>MyEvents</div>
-  )
-}
+  const { showToast } = useAppContext();
+  const { data: eventData } = useQuery("getMyEvents", apiClient.getMyEvents, {
+    onError: () => {
+      showToast({ message: "Couldn't get events", type: "ERROR" });
+    },
+  });
+  return <div>MyEvents</div>;
+};
 
-export default MyEvents
+export default MyEvents;
