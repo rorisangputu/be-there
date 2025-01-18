ebca82fb957cb165c3d4251bf8a525d5d267f371
Rorisang Putu <rorisangputu@gmail.com>
2025-01-18 13:37:28 +0200

    updating route for event details

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -39,7 +39,7 @@ function App() {
           }
         />
         <Route
-          path="/event/:id"
+          path="/events/:id"
           element={
             <Layout>
               <EventDetails />
